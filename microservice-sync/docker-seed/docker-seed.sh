cf86ecde63c5ee4d39a235cf4196de9e07339503
Bob Reselman <reselbob@gmail.com>
2019-08-03 00:24:38 -0700

    fixed docker typo

--- microservice-sync/docker-seed/docker-seed.sh
+++ microservice-sync/docker-seed/docker-seed.sh
@@ -5,7 +5,7 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 #Create the fortunes container image
 
-docker build -t fortunes -f ../fortunes/Dockerfile
+docker build -t fortunes -f ../fortunes/Dockerfile .
 
 docker tag fortunes localhost:5000/fortunes
 
@@ -13,35 +13,35 @@ docker push localhost:5000/fortunes
 
 #Create the scheduler container image
 
-docker build -t scheduler -f ../scheduler/Dockerfile
+docker build -t scheduler -f ../scheduler/Dockerfile .
 
 docker tag scheduler localhost:5000/scheduler
 
 docker push localhost:5000/scheduler
 
 #Create the sender container image
-docker build -t sender -f ../sender/Dockerfile
+docker build -t sender -f ../sender/Dockerfile .
 
 docker tag sender localhost:5000/sender
 
 docker push localhost:5000/sender
 
 #Create the targetmockserver container image
-docker build -t targetmockserver -f ../sender/targetmockserver
+docker build -t targetmockserver -f ../sender/targetmockserver .
 
 docker tag targetmockserver localhost:5000/targetmockserver
 
 docker push localhost:5000/targetmockserver
 
 #Create the testconsumer container image
-docker build -t testconsumer -f ../sender/testconsumer
+docker build -t testconsumer -f ../sender/testconsumer .
 
 docker tag testconsumer localhost:5000/testconsumer
 
 docker push localhost:5000/testconsumer
 
 #Create the users container image
-docker build -t users -f ../sender/users
+docker build -t users -f ../sender/users .
 
 docker tag users localhost:5000/users
 
