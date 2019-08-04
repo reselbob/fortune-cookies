8e194c32e468b88be272a2050ca1a79b9a5566d9
Bob Reselman <reselbob@gmail.com>
2019-08-03 20:53:22 -0700

    executing docker build directly against Dockerfile

--- microservice-sync/docker-seed.sh
+++ microservice-sync/docker-seed.sh
@@ -4,48 +4,71 @@
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 #Create the fortunes container image
+cd fortunes
 
-docker build -t fortunes -f ./fortunes/Dockerfile .
+docker build -t fortunes .
 
 docker tag fortunes localhost:5000/fortunes
 
 docker push localhost:5000/fortunes
 
+cd ..
 #Create the scheduler container image
 
-docker build -t scheduler -f ./scheduler/Dockerfile .
+cd scheduler
+
+docker build -t scheduler .
 
 docker tag scheduler localhost:5000/scheduler
 
 docker push localhost:5000/scheduler
 
+cd ..
+
 #Create the sender container image
-docker build -t sender -f ./sender/Dockerfile .
+cd sender
+
+docker build -t sender .
 
 docker tag sender localhost:5000/sender
 
 docker push localhost:5000/sender
 
+cd ..
+
 #Create the targetmockserver container image
-docker build -t targetmockserver -f ./targetmockserver/Dockerfile .
+cd targetmockserver
+
+docker build -t targetmockserver .
 
 docker tag targetmockserver localhost:5000/targetmockserver
 
 docker push localhost:5000/targetmockserver
 
+cd ..
+
 #Create the testconsumer container image
-docker build -t testconsumer -f ./testconsumer/Dockerfile .
+
+cd testconsumer
+
+docker build -t testconsumer .
 
 docker tag testconsumer localhost:5000/testconsumer
 
 docker push localhost:5000/testconsumer
 
+cd ..
+
 #Create the users container image
-docker build -t users -f ./users/Dockerfile .
+cd users
+
+docker build -t users -f .
 
 docker tag users localhost:5000/users
 
 docker push localhost:5000/users
+
+cd ..
 
 #List the images in the registry
 
