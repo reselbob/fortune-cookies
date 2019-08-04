b766860e44527eca0ee1f57b7deda07b0526539a
Bob Reselman <reselbob@gmail.com>
2019-08-03 23:24:21 -0700

    reorganizing k8s build

--- microservice-sync/kubernetes/generate-k8s-resources.sh
+++ microservice-sync/kubernetes/generate-k8s-resources.sh
@@ -7,24 +7,21 @@ for filename in ./manifests/targets/*-pod.yaml; do
     kubectl apply -f "$filename"
 done
 
+kubectl apply -f ./manifests/sender-pod.yaml
+kubectl apply -f ./manifests/scheduler-pod.yaml
+kubectl apply -f ./manifests/users-pod.yaml
+kubectl apply -f ./manifests/fortunes-pod.yaml
+
+sleep 5
+
 #then targets services
 for filename in ./manifests/targets/*-service.yaml; do
     kubectl apply -f "$filename"
 done
 
 
-#generate sender
-kubectl apply -f ./manifests/sender-pod.yaml
+
 kubectl apply -f ./manifests/sender-service.yaml
-
-#generate scheduler
-kubectl apply -f ./manifests/scheduler-pod.yaml
 kubectl apply -f ./manifests/scheduler-service.yaml
-
-#generate users
-kubectl apply -f ./manifests/users-pod.yaml
 kubectl apply -f ./manifests/users-service.yaml
-
-#generate fortunes
-kubectl apply -f ./manifests/fortunes-pod.yaml
 kubectl apply -f ./manifests/fortunes-service.yaml
