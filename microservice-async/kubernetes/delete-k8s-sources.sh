493e7417caa5c63b1e770cdb89d6adff52ab69f5
Bob Reselman <reselbob@gmail.com>
2019-08-12 22:10:09 -0700

    building out delete k8s

--- microservice-async/kubernetes/delete-k8s-sources.sh
+++ microservice-async/kubernetes/delete-k8s-sources.sh
@@ -4,8 +4,12 @@
 kubectl delete service scheduler
 kubectl delete service sender
 kubectl delete service fortunes
+kubectl delete service users
+kubectl delete service message-gen
 
 kubectl delete pod pingrx
 kubectl delete pod scheduler
 kubectl delete pod sender
 kubectl delete pod fortunes
+kubectl delete pod users
+kubectl delete pod message-gen
