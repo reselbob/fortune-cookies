d4a7bbb4d6ca3bf5174cb0705be4dd827a4a07c0
Bob Reselman <reselbob@gmail.com>
2021-01-10 16:49:34 -0800

    fixing docker-compose bug

--- monolith_strangled/report_gen/index.js
+++ monolith_strangled/report_gen/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const {getSentFortunes} = require('./dataManager');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 app.get("/", async (req, res) => {
     const fortunes = await getSentFortunes();
